f18baca3d8773daa54a99dae1fdc91f312cd1066
Lorenzo G <lorenzo.gurgo@gmail.com>
2023-02-08 00:03:41 +0100

    fonction read_and_store_tsv pour lire et envoyer les fichier

--- BDD.py
+++ BDD.py
@@ -18,20 +18,20 @@ print('URL', url)
 #création de la base de données avec l'url et le fichier config.yaml
 engine = create_engine(url)
 
-# lit les fichiers tsv depuis l'url et les stockent dans des dataframes
-names = pd.read_csv('https://datasets.imdbws.com/name.basics.tsv.gz', compression='gzip', sep='\t', nrows=1000)
-title = pd.read_csv('https://datasets.imdbws.com/title.akas.tsv.gz', compression='gzip', sep='\t', nrows=1000)
-basics = pd.read_csv('https://datasets.imdbws.com/title.basics.tsv.gz', compression='gzip', sep='\t', nrows=1000)
-crew = pd.read_csv('https://datasets.imdbws.com/title.crew.tsv.gz', compression='gzip', sep='\t', nrows=1000)
-episode = pd.read_csv('https://datasets.imdbws.com/title.episode.tsv.gz', compression='gzip', sep='\t', nrows=1000)
-principals = pd.read_csv('https://datasets.imdbws.com/title.principals.tsv.gz', compression='gzip', sep='\t', nrows=1000)
-ratings = pd.read_csv('https://datasets.imdbws.com/title.ratings.tsv.gz', compression='gzip', sep='\t', nrows=1000)
-
-# envoie les dataframes dans la base de données
-names.to_sql('names', engine, if_exists='replace')
-title.to_sql('title_akas', engine, if_exists='replace')
-basics.to_sql('title_basics', engine, if_exists='replace')
-crew.to_sql('title_crew', engine, if_exists='replace')
-principals.to_sql('title_principals', engine, if_exists='replace')
-ratings.to_sql('title_ratings', engine, if_exists='replace')
-episode.to_sql('title_episode', engine, if_exists='replace')
+# Permet de lire les fichiers tsv depuis l'url et les stockent dans des dataframes puis envoie les dataframes dans la base de données
+def read_and_store_tsv(url, file_name, engine, nrows=1000):
+    df = pd.read_csv(url, compression='gzip', sep='\t', nrows=nrows)
+    df.to_sql(file_name, engine, if_exists='replace')
+# liste des fichiers tsv à télécharger
+files = [
+    ('https://datasets.imdbws.com/name.basics.tsv.gz', 'names'),
+    ('https://datasets.imdbws.com/title.akas.tsv.gz', 'title_akas'),
+    ('https://datasets.imdbws.com/title.basics.tsv.gz', 'title_basics'),
+    ('https://datasets.imdbws.com/title.crew.tsv.gz', 'title_crew'),
+    ('https://datasets.imdbws.com/title.episode.tsv.gz', 'title_episode'),
+    ('https://datasets.imdbws.com/title.principals.tsv.gz', 'title_principals'),
+    ('https://datasets.imdbws.com/title.ratings.tsv.gz', 'title_ratings'),
+]
+# boucle pour lire et stocker les fichiers tsv dans la base de données
+for url, file_name in files:
+    read_and_store_tsv(url, file_name, engine)
