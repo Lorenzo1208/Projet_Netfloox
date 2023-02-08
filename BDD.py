8d3b0cb43e3c05adddc0cc87f9cd6961a36431b3
Lorenzo G <lorenzo.gurgo@gmail.com>
2023-02-08 14:06:57 +0100

    update fonction replace \N par None

--- BDD.py
+++ BDD.py
@@ -1,6 +1,8 @@
 from sqlalchemy import create_engine
 import pandas as pd
+import sqlalchemy
 import yaml
+import numpy as np
 
 # ouvre le fichier yaml en mode lecture seule et le charge avec yaml.safe_load
 with open('config.yaml', 'r') as file:
@@ -19,10 +21,12 @@ print('URL', url)
 engine = create_engine(url)
 
 # Permet de lire les fichiers tsv depuis l'url et les stockent dans des dataframes puis envoie les dataframes dans la base de données
-def read_and_store_tsv(url, file_name, engine, nrows=10000, chunksize=10000):
+def read_and_store_tsv(url, file_name, engine, nrows=1000, chunksize=1000):
     chunks = pd.read_csv(url, compression='gzip', sep='\t', nrows=nrows, chunksize=chunksize, low_memory=False)
     for chunk in chunks:
-        chunk.to_sql(file_name, engine, if_exists='replace', index=False)
+        chunk = chunk.applymap(lambda x: None if x == r'\N' else x)
+        chunk = chunk.astype({col: int if chunk[col].astype(str).str.isnumeric().all() else 'object' for col in chunk.columns})
+        chunk.to_sql(file_name, engine, if_exists='replace', index=False, dtype={col: sqlalchemy.types.String(255) if chunk[col].dtype == 'object' else sqlalchemy.types.INTEGER() for col in chunk.columns})
 
 # liste des fichiers tsv à télécharger
 files = [
