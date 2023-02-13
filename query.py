09c99d7cd68f4e1b92c1a469c7f25e35b0ec1b75
Lorenzo G <lorenzo.gurgo@gmail.com>
2023-02-13 18:42:50 +0100

    update bb_load and ajout fonction dans query

--- query.py
+++ query.py
@@ -1,28 +1,40 @@
-from sqlalchemy import create_engine, text
-import pandas as pd
-import sqlalchemy
 import yaml
-import traceback
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+from sqlalchemy import create_engine, text
 
-# Load the YAML configuration file
-with open('config.yaml', 'r') as file:
-    config = yaml.safe_load(file)
+def get_config():
+    with open('config.yaml', 'r') as file:
+        config = yaml.safe_load(file)
+    return config
 
-# Get the MySQL configuration
-cfg = config['mysql-datalab']
+def create_engine_from_config(config):
+    cfg = config['mysql']
+    return create_engine("{driver}://{user}:{password}@{host}/{database}".format(**cfg))
 
-# Create the database connection URL
-url = "{driver}://{user}:{password}@{host}/{database}".format(**cfg)
+def get_data(engine, query):
+    with engine.connect() as conn:
+        df = conn.execute(query)
+        df = pd.DataFrame(df.fetchall())
+        df.columns = df.columns = df.keys()
+    return df
 
-# Create the database engine
-engine = create_engine(url)
+def plot_data(df):
+    plt.figure(figsize=(20,10))
+    sns.barplot(data=df, x="birthYear", y="N")
+    plt.xlabel("Birth Year")
+    plt.ylabel("Count")
+    plt.title("Count of Names by Birth Year")
+    plt.show()
 
+def main():
+    config = get_config()
+    engine = create_engine_from_config(config)
+    query = text("""SELECT * FROM title_basics;""")
+    query2 = text("""SELECT birthYear, count(*) AS N FROM names GROUP BY birthYear;""")
+    df = get_data(engine, query2)
+    plot_data(df)
 
-query = text("""SELECT * FROM title_basics LIMIT 10
-""")
-
-# Execute the query using the engine
-with engine.connect() as con:
-    result = con.execute(query)
-    for row in result:
-        print(row)
+if __name__ == '__main__':
+    main()
