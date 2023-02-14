5127840a369218e06f82f2c05248fd12dc44a527
Lorenzo G <55632146+Lorenzo1208@users.noreply.github.com>
2023-02-14 11:22:12 +0100

    Update Script_all.sql

--- Script_all.sql
+++ Script_all.sql
@@ -6,7 +6,7 @@ ALTER TABLE bdd.names MODIFY COLUMN primaryProfession varchar(66) CHARACTER SET 
 ALTER TABLE bdd.knownfortitles MODIFY COLUMN knownForTitles varchar(64) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL;
 ALTER TABLE bdd.knownfortitles MODIFY COLUMN nconst varchar(10) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL;
 ALTER TABLE bdd.title_akas MODIFY COLUMN titleId varchar(10) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL;
-ALTER TABLE bdd.title_akas MODIFY COLUMN title varchar(656) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL;
+ALTER TABLE bdd.title_akas MODIFY COLUMN title varchar(1500) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL;
 ALTER TABLE bdd.title_akas MODIFY COLUMN region varchar(4) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL;
 ALTER TABLE bdd.title_akas MODIFY COLUMN `language` varchar(3) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL;
 ALTER TABLE bdd.title_akas MODIFY COLUMN types varchar(20) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL;
@@ -35,37 +35,17 @@ ALTER TABLE bdd.title_principals MODIFY COLUMN job varchar(290) CHARACTER SET ut
 ALTER TABLE bdd.title_principals MODIFY COLUMN `characters` varchar(1308) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL;
 ALTER TABLE bdd.title_ratings MODIFY COLUMN tconst varchar(10) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL;
 ALTER TABLE bdd.title_ratings MODIFY COLUMN averageRating INT NULL;
-
 DELETE FROM bdd.directors
 WHERE directors IS NULL;
-
 ALTER TABLE bdd.directors ADD CONSTRAINT directors_pk PRIMARY KEY (tconst,directors);
-
-SELECT directors
-FROM bdd.directors
-WHERE directors IS NULL;
-
 DELETE FROM bdd.knownfortitles
 WHERE knownfortitles IS NULL;
-
-SELECT knownfortitles
-FROM bdd.knownfortitles
-WHERE knownfortitles IS NULL;
-
 ALTER TABLE bdd.knownfortitles ADD CONSTRAINT knownfortitles_pk PRIMARY KEY (nconst,knownForTitles);
-
 ALTER TABLE bdd.names ADD CONSTRAINT names_pk PRIMARY KEY (nconst);
-
 ALTER TABLE bdd.title_akas ADD CONSTRAINT title_akas_pk PRIMARY KEY (titleId,`ordering`);
-
 ALTER TABLE bdd.title_basics ADD CONSTRAINT title_basics_pk PRIMARY KEY (tconst);
-
 ALTER TABLE bdd.title_episode ADD CONSTRAINT title_episode_pk PRIMARY KEY (tconst);
-
 ALTER TABLE bdd.title_principals ADD CONSTRAINT title_principals_pk PRIMARY KEY (tconst,`ordering`);
-
 ALTER TABLE bdd.title_ratings ADD CONSTRAINT title_ratings_pk PRIMARY KEY (tconst);
-
 DELETE FROM bdd.writers WHERE writers  IS NULL;
-
 ALTER TABLE bdd.writers ADD CONSTRAINT writers_pk PRIMARY KEY (tconst,writers);
