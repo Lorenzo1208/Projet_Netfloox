df60b506cac6dfc96f739314528ec228385854ff
Lorenzo G <lorenzo.gurgo@gmail.com>
2023-02-21 15:17:56 +0100

    ajout recommendation 5 films

--- Netfloox/Netfloox/urls.py
+++ Netfloox/Netfloox/urls.py
@@ -15,9 +15,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from app.views import home
+from app.views import home, analyse, prediction
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("", home , name="home"), 
+    path("", home , name="home"),
+    path("analyse", analyse , name="analyse"),
+    path("prediction", prediction , name="prediction"),
 ]
--- Netfloox/app/views.py
+++ Netfloox/app/views.py
@@ -1,6 +1,58 @@
 from django.shortcuts import render
+import pandas as pd
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.metrics.pairwise import cosine_similarity
+from sklearn.decomposition import TruncatedSVD
+import numpy as np
+import os
 
-# Create your views here.
+# Get the current working directory
+cwd = os.getcwd()
+print(f"Current working directory: {cwd}")
+
+# List the files in the current working directory
+files = os.listdir(cwd)
+print(f"Files in directory: {files}")
 
 def home(request):
     return render(request, 'home.html')
+
+def analyse(request):
+    return render(request, 'analyse.html')
+
+def load_data(csv_file):
+    return pd.read_csv(csv_file, encoding='utf-8')
+
+def get_similar_movies(movie_title, data, cv, count_matrix, svd, n_similar=5):
+    liked_movie_idx = data[data['originalTitle'] == movie_title].index
+
+    if len(liked_movie_idx) > 0:
+        cosine_sim_light = cosine_similarity(X=count_matrix[liked_movie_idx], Y=count_matrix)
+        row = cosine_sim_light[0]
+        indices = np.argsort(-row)[1:n_similar+1]
+
+        movies = []
+        for i in indices:
+            movie = data.iloc[i]['originalTitle']
+            movies.append((movie, row[i]))
+    else:
+        movies = [(f"Aucun film trouvé avec le titre '{movie_title}'.", 0)]
+
+    return movies
+
+def prediction(request):
+    data = load_data('app/cosine_features_no_date.csv')
+
+    cv = CountVectorizer()
+    count_matrix = cv.fit_transform(data['features']).tocsr()
+    svd = TruncatedSVD(n_components=1)
+    count_matrix_svd = svd.fit_transform(count_matrix)
+
+    liked_movie = request.GET.get('movie', 'The Matrix')
+    movies = get_similar_movies(liked_movie, data, cv, count_matrix, svd)
+
+    context = {
+        'movies': movies,
+        'liked_movie': liked_movie,
+    }
+    return render(request, 'prediction.html', context)
